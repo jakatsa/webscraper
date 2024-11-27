8dee2d6d6ced7f476d5783d3a7c4730e2a14a7b5
Joe <jakatsa5@gmail.com>
2024-11-27 19:39:54 +0300

    getting html from url

--- html_scraper/ecomm_scrapper.py
+++ html_scraper/ecomm_scrapper.py
@@ -3,9 +3,27 @@ import requests
 import csv
 import bs4
 
+USER_AGENT='Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36 Edg/131.0.2903.51'
+REQUEST_HEADER={
+    'User-Agent':USER_AGENT,
+    'Accept-Language':'en-US, en;q=0.5'
+}
+#getting html for the page 
+def get_page_html(url):
+    res=requests.get(url=url,headers=REQUEST_HEADER)
+    return res.content 
+
+#extracting urls info.
+def extract_product_info(url):
+    product_info={}
+    print(f'Scraping URL:{url}')
+    html=get_page_html(url=url)
+    print(html)
+
+#html sscraping script 
 if __name__ =="__main__":
     with open('amazon_products_urls.csv',newline='') as csvfile:
         reader=csv.reader(csvfile,delimiter=',')
         for row in reader:
             url=row[0]
-            print(url)
+            print(extract_product_info(url))
